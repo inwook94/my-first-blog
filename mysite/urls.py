2b5378e0923e3c96e355b7b7d70899bf534c1959
inwook <inwook94@nate.com>
2021-08-05 21:59:57 +0900

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+
+    # admin을 제외한 나머지는 blog.urls로 가도록 하라.
+    # include뒤에 작성하는 것은 모듈명을 뜻하는 것, 즉 blog라는 패키지 안에 들어있는 urls 모델을 사용하라.
+    # 여기서 include는 blog.urls로 그대로 들어가 blog.urls에 있는 정규표현식을 적용한다.
+    url(r'', include('blog.urls'))
 ]
